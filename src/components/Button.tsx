47952b828590f931c397d34a4004749edb13fc1c
Ben Pluim <benpluim@Bens-2010-MacBook-Pro.local>
2023-01-23 14:00:34 -0700

    feat: add Github Actions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,11 +6,11 @@ export interface ButtonProps {
 
 export const Button = styled.button<ButtonProps>`
   /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
+  background: ${props => props.primary ? "" : "white"};
+  color: ${props => props.primary ? "white" : ""};
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid ;
   border-radius: 3px;
 `;
